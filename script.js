7557191701cb2d5cf20a4707bb33f0546e6ce762
Noora AlDraib <aldraibnora@gmail.com>
2023-09-04 18:54:16 +0200

    done

---
+++ script.js
@@ -0,0 +1,22 @@
+alert( "welcome to my website! my name is Noora, I am going to calculate ur grade");
+let grade = prompt( " enter ur grade");
+console.log(grade);
+
+if(grade <=100 && grade>=90 ){
+    console.log("ur grade is A")
+}
+else if(grade<=89 && grade>80){
+    console.log("ur grade is B")
+}
+else if(grade<=79 && grade>70){
+    console.log("ur grade is C")
+}
+else if(grade<=69 && grade>60){
+    console.log("ur grade is D")
+}
+else{
+console.log("ur grade is F")
+}
+console.log("%cthis text is red", "color: red")
+// javascript is not java 
+
